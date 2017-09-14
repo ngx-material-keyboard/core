4b479954f514b0e401bd97e3366bd98c33d9b1a6
Arjan Kempes <arjan@7lab.nl>
2017-09-14 16:11:24 +0200

    Small fix

--- src/components/keyboard-key/keyboard-key.component.ts
+++ src/components/keyboard-key/keyboard-key.component.ts
@@ -108,7 +108,7 @@ export class MdKeyboardKeyComponent implements OnInit {
         break;
 
       case 'Bksp':
-        this.ngControl.control.setValue([value.slice(0, caret - 1), value.slice(caret)].join(''), { onlySelf: true, emitEvent: true });
+        this.ngControl.control.setValue([value.slice(0, caret - 1), value.slice(caret)].join(''), { onlySelf: false, emitEvent: true });
         this._setCursorPosition(caret - 1);
         break;
 
@@ -138,7 +138,7 @@ export class MdKeyboardKeyComponent implements OnInit {
     }
 
     if (char && this.input) {
-      this.ngControl.control.setValue([value.slice(0, caret), char, value.slice(caret)].join(''), { onlySelf: true, emitEvent: true });
+      this.ngControl.control.setValue([value.slice(0, caret), char, value.slice(caret)].join(''), { onlySelf: false, emitEvent: true });
       this._setCursorPosition(caret + 1);
     }
   }
@@ -193,7 +193,7 @@ export class MdKeyboardKeyComponent implements OnInit {
       return;
     }
 
-    this.ngControl.control.setValue(this.ngControl.value, { onlySelf: true, emitEvent: true });
+    this.ngControl.control.setValue(this.ngControl.value, { onlySelf: false, emitEvent: true });
     // ^ this is used to not only get "focus", but
     // to make sure we don't have it everything -selected-
     // (it causes an issue in chrome, and having it doesn't hurt any other browser)
