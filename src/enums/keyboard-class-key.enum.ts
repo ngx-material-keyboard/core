277717595bc339b3726c44f9b61024b6a0abdb57
David Enke <post@davidenke.de>
2017-09-22 17:14:02 +0200

    due to build propblems (with rollup?) we still can't use string based enums yet

--- src/enums/keyboard-class-key.enum.ts
+++ src/enums/keyboard-class-key.enum.ts
@@ -1,3 +1,7 @@
+// TODO: use real string based enums (available sine typescript 1.4) if
+// [tslint](https://github.com/palantir/tslint/issues/2993) and more important
+// [rollup](https://github.com/angular/angular/issues/17516) support it
+/*
 export enum KeyboardClassKey {
   Alt = 'Alt',
   AltGr = 'AltGr',
@@ -8,4 +12,16 @@ export enum KeyboardClassKey {
   Shift = 'Shift',
   Space = 'Space',
   Tab = 'Tab'
+}
+*/
+export enum KeyboardClassKey {
+  Alt = 'Alt' as any,
+  AltGr = 'AltGr' as any,
+  AltLk = 'AltLk' as any,
+  Bksp = 'Bksp' as any,
+  Caps = 'Caps' as any,
+  Enter = 'Enter' as any,
+  Shift = 'Shift' as any,
+  Space = 'Space' as any,
+  Tab = 'Tab' as any
 }
