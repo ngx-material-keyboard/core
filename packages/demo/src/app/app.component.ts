07ef34e55b65d555d64b49243600818814a96a58
David Enke <post@davidenke.de>
2018-02-20 01:07:12 +0100

    prepared little testbed

--- packages/demo/src/app/app.component.ts
+++ packages/demo/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, Inject, LOCALE_ID, OnDestroy, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, NgForm } from '@angular/forms';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 import { IKeyboardLayout, MAT_KEYBOARD_LAYOUTS, MatKeyboardComponent, MatKeyboardRef, MatKeyboardService } from '@ngx-material-keyboard/core';
 
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
 @Component({
@@ -16,6 +18,12 @@ export class AppComponent implements OnInit, OnDestroy {
   private _enterSubscription: Subscription;
 
   private _keyboardRef: MatKeyboardRef<MatKeyboardComponent>;
+
+  private _submittedForms = new BehaviorSubject<{ control: string, value: string }[][]>([]);
+
+  get submittedForms(): Observable<{ control: string, value: string }[][]> {
+    return this._submittedForms.asObservable();
+  }
 
   darkTheme: boolean;
 
@@ -31,6 +39,10 @@ export class AppComponent implements OnInit, OnDestroy {
     name: string;
     layout: IKeyboardLayout;
   }[];
+
+  testModelValue = 'Sushi';
+
+  testControlValue = new FormControl({ value: 'Emmentaler', disabled: false });
 
   get keyboardVisible(): boolean {
     return this._keyboardService.isOpened;
@@ -56,7 +68,15 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   submitForm(form?: NgForm) {
-    console.log('submit form', form);
+    const submittedForms = this._submittedForms.getValue();
+    const submittedForm = Object
+      .keys(form.controls)
+      .map((control: string) => ({
+        control,
+        value: form.controls[control].value
+      }));
+    submittedForms.push(submittedForm);
+    this._submittedForms.next(submittedForms);
   }
 
   openKeyboard(locale = this.defaultLocale) {
