4dfe61201c6e8abecf08388c4ae4d4bf177d49eb
David Enke <post@davidenke.de>
2018-10-01 11:21:03 +0200

    use lerna, moved testbeds

---
+++ packages/core/index.ts
@@ -0,0 +1,31 @@
+export * from './src/classes/keyboard-ref.class';
+
+export * from './src/components/keyboard/keyboard.component';
+export * from './src/components/keyboard-container/keyboard-container.component';
+export * from './src/components/keyboard-key/keyboard-key.component';
+
+export * from './src/configs/keyboard.config';
+export * from './src/configs/keyboard-deadkey.config';
+export * from './src/configs/keyboard-icons.config';
+export * from './src/configs/keyboard-layouts.config';
+
+export * from './src/directives/keyboard.directive';
+
+export * from './src/enums/keyboard-class-key.enum';
+export * from './src/enums/keyboard-modifier.enum';
+export * from './src/enums/keyboard-animation-state.enum';
+export * from './src/enums/keyboard-animation-transition.enum';
+
+export * from './src/interfaces/keyboard-deadkeys.interface';
+export * from './src/interfaces/keyboard-icons.interface';
+export * from './src/interfaces/keyboard-layout.interface';
+export * from './src/interfaces/keyboard-layouts.interface';
+export * from './src/interfaces/locale-map.interface';
+
+export * from './src/pipes/kebab-case.pipe';
+
+export * from './src/services/keyboard.service';
+
+export * from './src/utils/keyboard.utils';
+
+export * from './src/keyboard.module';
